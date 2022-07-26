cf2771e379fa58a635bdbf9cffd39dace1588cf1
nilrem3 <nilremnilrem@gmail.com>
2022-07-25 18:42:19 -0700

    Add rendering thread
    https://trello.com/c/2MWt0oXD/9-rendering-thread

---
+++ src/frontend/main.rs
@@ -0,0 +1,5 @@
+
+//! This is the "main function" for the rendering thread.  This is called once from main and everything else happens here.
+pub fn main(){
+    println!("Rendering Thread Started");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::{thread};
+
 mod frontend;
 mod backend;
 
-fn main() {}
+fn main() {
+    
+    println!("Main Thread Started");
+
+    //start rendering thread
+    thread::spawn( || {
+        frontend::main::main();
+    });
+
+}
 
 #[cfg(test)]
 mod test{
